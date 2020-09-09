6572b6e052f2fc1db858b45e802516fbb85684f9
Oliver Cho <olivercho@Olivers-MacBook-Pro.local>
2020-09-08 17:38:19 -0700

    GUI Completed

--- gui.java
+++ gui.java
@@ -1,46 +1,116 @@
-import javax.swing.*;
 import java.awt.GridLayout;
-import java.awt.ActionListener;
-import java.awt.ActionEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class gui extends JPanel{
+  JButton buttons[] = new JButton[9];
+  private String currentPlayerMark;
+
   public gui(){
-      setLayout(new GridLayout(3,3));
-      initializebuttons();
-      JFrame window = new JFrame("Tic-Tac-Toe");
-      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      window.getContentPane().add(new gui());
-      window.setBounds(300,200,300,300);
-      window.setVisible(true);
+    currentPlayerMark="X";
+    setLayout(new GridLayout(3,3));
+    initializeButtons();
   }
 
-  public void initializebuttons(){
-      for(int i = 0; i <= 8; i++){
-        buttons[i] = new JButton();
-        buttons[i].setText("");
-        buttons[i].addActionListener(new buttonListener());
-        add(buttons[i]);
-      }
+  public void initializeButtons(){
+    for(int i = 0; i <= 8; i++){
+      buttons[i] = new JButton();
+      buttons[i].setLabel(Integer.toString(i));
+      buttons[i].setText("-");
+      buttons[i].addActionListener(new buttonListener());
+      add(buttons[i]);
+    }
   }
 
   public void resetButtons(){
-      for(int i = 0; i <= 8; i++){
-        buttons[i].setText("");
-      }
+    for(int i = 0; i <= 8; i++){
+      buttons[i].setText("-");
+    }
   }
 
-  public void actionPerformed(ActionEvent e) {
-      JButton buttonClicked = (JButton)e.getSource(); //get the particular button that was clicked
-      if(alternate%2 == 0)
-        buttonClicked.setText("X");
-      else
-        buttonClicked.setText("O");
+  private class buttonListener implements ActionListener{
+    public void actionPerformed(ActionEvent e) {
+        JButton buttonClicked = (JButton)e.getSource(); //get the particular button that was clicked
+        buttonClicked.setText(currentPlayerMark);
 
-      if(checkForWin() == true){
-        JOptionPane.showConfirmDialog(null, "Game Over.");
-        resetButtons();
+        if (currentPlayerMark=="X"){
+          currentPlayerMark="O";
+        }
+        else{
+          currentPlayerMark="X";
+        }
+
+        if(checkForWin() == true)
+            {
+                JOptionPane.showConfirmDialog(null, "Game Over.");
+                resetButtons();
+            }
+
+        int sum=0;
+        for(int i = 0; i <= 8; i++){
+          if(buttons[i].getText()=="-"){
+            sum++;
+          }
+        }
+        if(sum==0){
+          JOptionPane.showConfirmDialog(null, "Game Over.");
+          resetButtons();
+        }
+    }
+    public boolean checkForWin(){
+      /**   Reference: the button array is arranged like this as the board
+       *      0 | 1 | 2
+       *      3 | 4 | 5
+       *      6 | 7 | 8
+       */
+      if(checkRowForWin()==true){
+        return true;
       }
-
-      alternate++;
+      else if(checkColumnForWin()==true){
+        return true;
+      }
+      else if(checkDiagonalForWin()==true){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    public boolean checkRowForWin(){
+      boolean result=false;
+      for(int i=0;i<=6;i=i+3){
+        if((buttons[i].getText()==buttons[i+1].getText())&&(buttons[i+1].getText()==buttons[i+2].getText())&&(buttons[i].getText()!="-")){
+          result=true;
+        }
+      }
+      return result;
+    }
+    public boolean checkColumnForWin(){
+      boolean result=false;
+      for(int i=0;i<=2;i=i+1){
+        if((buttons[i].getText()==buttons[i+3].getText())&&(buttons[i+3].getText()==buttons[i+6].getText())&&(buttons[i].getText()!="-")){
+          result=true;
+        }
+      }
+      return result;
+    }
+    public boolean checkDiagonalForWin(){
+      boolean result=false;
+      if((buttons[0].getText()==buttons[4].getText())&&(buttons[4].getText()==buttons[8].getText())&&(buttons[0].getText()!="-")){
+        result=true;
+      }
+      if((buttons[2].getText()==buttons[4].getText())&&(buttons[4].getText()==buttons[6].getText())&&(buttons[2].getText()!="-")){
+        result=true;
+      }
+      return result;
+    }
+  }
+  public static void main(String[] args){
+    JFrame window = new JFrame("Tic-Tac-Toe");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.getContentPane().add(new gui());
+    window.setBounds(300,200,300,300);
+    window.setVisible(true);
   }
 }
